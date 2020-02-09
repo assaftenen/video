6b1c789c9c0c88b7f61557ab66cde3a0f0ba2831
assaf ten <assaften@artlist.io>
2020-02-09 22:29:07 +0200

    init struct | layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { VideoItemComponent } from './containers/video-item/video-item.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/:id', component: VideoItemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VidoItemComponent } from './containers/vido-item/vido-item.component';
+import { PlayerComponent } from './components/player/player.component';
+import { VideoItemComponent } from './containers/video-item/video-item.component';
+import { TranscriptComponent } from './containers/transcript/transcript.component';
+import { VideoTitleComponent } from './components/video-title/video-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VidoItemComponent,
+    PlayerComponent,
+    VideoItemComponent,
+    TranscriptComponent,
+    VideoTitleComponent
   ],
   imports: [
     BrowserModule,
